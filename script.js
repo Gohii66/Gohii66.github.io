ddeaed2ccf92bfe3bab75c6c16a5acc8219f27d1
Gohii66 <meszarosmilan66@gmail.com>
2022-03-23 22:40:14 +0100

    összes fájl feltöltve

---
+++ script.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+  $(".karrier a").click(
+    function(event) {
+       event.preventDefault();
+    }
+ );
+});
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
